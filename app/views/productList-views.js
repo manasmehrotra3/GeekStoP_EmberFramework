0dd8caeec46aebdcd764d3b436449cad3d869ea3
Manas Mehrotra <manasmehrotra3@gmail.com>
2012-07-30 16:05:11 +0530

    Binding Corrected
    Corrected image binding by introducing id to the target class

--- app/views/productList-views.js
+++ app/views/productList-views.js
@@ -6,53 +6,43 @@ define([
 	'app/models/item'
 ], function(em, utils, itemControllers, listTemplateSource, Item) {
 
-			var CreateItemView = em.View.extend({
-					  click: function(evt) {
-					  em.MyApp.get("mainView").productDetail();
-					  }
-					});
+			my_data= [{url: 'style/Do not Lose Your Mind, Lose Your Weight.JPG',id: 'Do not Lose Your Mind, Lose Your Weight'}, {url: 'style/The Immortals of Meluha.JPG', id: 'The Immortals of Meluha'}, {url: 'style/The Secret of the Nagas.JPG', id: 'The Secret of the Nagas'}, {url: 'style/Chanakya Chant.JPG', id: 'Chanakya Chant'}, {url: 'style/I have a Dream.JPG', id: 'I have a Dream'}, {url: 'style/The Fountainhead.JPG', id: 'The Fountainhead'}, {url: 'style/Darkly Dreaming Dexter.JPG', id: 'Darkly Dreaming Dexter'}, {url: 'style/The Hobbit.JPG', id: 'The Hobbit'}];
+
+			em.User = Em.Object.extend({
+    			url: null,
+    			id: null,
+			});
+			
+			em.usersController = Em.ArrayController.create({
+    			content: [],
+    			initWithDataFromServer: function(hash){
+        		var users = hash.map(function(item){
+            	return em.User.create({
+                url: item.url,
+                id: item.id,
+                  });
+               });
+              this.pushObjects(users);
+              }
+            });
+
+       		em.usersController.initWithDataFromServer(my_data);
+			
+			
 					
 			var ListItemsView = em.View.extend({
-				contentBooks: ["Do not Lose Your Mind, Lose Your Weight","The Immortals of Meluha","The Secret of the Nagas","Chanakya Chant","I have a Dream","The Fountainhead","Darkly Dreaming Dexter","The Hobbit"],
 				contentMobiles: ['Samsung Galaxy Tab 750','Motorola Defy (Black)','Samsung Galaxy Note','Motorola RAZR XT910 (Mercury Silver)','Sony Ericsson Xperia Arc S (Black)','Samsung Galaxy Ace S5830 (Onyx Black)','HTC Wildfire S (Dark Grey)','Micromax Superfone Lite A75 (Charcoal Black)'],
 				template: em.Handlebars.compile(listTemplateSource),
 			});
 			
-			em.PhotoListController = em.ArrayProxy.create ({
-				content: [],
-				selected: null,
-			});
-			
-			em.SelectedPhotoController = em.Object.create ({
-				contentBinding: 'em.PhotoListController.selected'
-			});
-			
-			em.myBook = em.Object.create({
-                title: 'style/Do not Lose Your Mind, Lose Your Weight.JPG',
-            });
-            
-            em.myMobile = em.Object.create({
-                title: 'style/Motorola Defy (Black).JPG',
-            });
-			
 			em.ThumbnailPhotoView = em.View.extend({
-			   Books: function() {
-			   	    return em.myBook.title
-				}.property(),
-				
-			   Mobiles: function() {
-			   	    return em.myMobile.title
-				}.property(),
-				
-               click: function(evt) {
-               em.PhotoListController.set('selected', this.get('content'));
-               alert(em.PhotoListController.selected);
-               //em.MyApp.get("mainView").productDetail();
-               },
+			   edit: function(evt) {
+                 em.MyApp.item.name= event.target.id;
+                 em.MyApp.get("mainView").productDetail();
+                 },	
 			});
 			
 			return {
-				CreateItemView: CreateItemView,
 				ListItemsView: ListItemsView
 			}
 		}
